96426db373cf9a5b44bf59833141f3f0b6a3473a
AnkithRD <rdankith@gmail.com>
2018-02-19 09:55:15 -0500

    Least common ancestor

---
+++ src/org/learning/binarysearchtrees/BinarySearchTree2.java
@@ -0,0 +1,45 @@
+package org.learning.binarysearchtrees;
+
+import org.learning.support.TreeNode;
+
+public class BinarySearchTree2 {
+
+	TreeNode findLCA(TreeNode root, int data1, int data2) {
+		
+		if(root == null) return null;
+		
+		if(root.getData() == data1 || root.getData() == data2) return root;
+		
+		if(Math.max(data1, data2) < root.getData()) {
+			return findLCA(root.getLeft(), data1, data2);
+		}else if(Math.min(data1, data2) > root.getData()) {
+			return findLCA(root.getRight(), data1, data2);
+		}else {
+			return root;
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		BinarySearchTree2 t = new BinarySearchTree2();
+		
+		TreeNode root = new TreeNode(6);
+		TreeNode inner1 = new TreeNode(5);
+		TreeNode inner2 = new TreeNode(10);
+		TreeNode leaf1 = new TreeNode(3);
+		TreeNode leaf2 = new TreeNode(4);
+		TreeNode leaf3 = new TreeNode(9);
+		TreeNode leaf4 = new TreeNode(11);
+		
+		root.setLeft(inner1);
+		root.setRight(inner2);
+		inner1.setLeft(leaf1);
+		inner1.setRight(leaf2);
+		inner2.setLeft(leaf3);
+		inner2.setRight(leaf4);
+		
+		System.out.println("Least common Ancestor");
+		System.out.println(t.findLCA(root, 9, 11).getData());
+	}
+
+}
