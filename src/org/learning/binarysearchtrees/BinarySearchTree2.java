f34be94db16532a4e0316d6bf095f990616f94ab
AnkithRD <rdankith@gmail.com>
2018-02-19 10:20:44 -0500

    Validate binary search tree

--- src/org/learning/binarysearchtrees/BinarySearchTree2.java
+++ src/org/learning/binarysearchtrees/BinarySearchTree2.java
@@ -1,6 +1,6 @@
 package org.learning.binarysearchtrees;
 
-import org.learning.support.TreeNode;
+import org.learning.support.TreeNode;import com.sun.org.apache.xerces.internal.impl.dv.ValidatedInfo;
 
 public class BinarySearchTree2 {
 
@@ -19,15 +19,21 @@ public class BinarySearchTree2 {
 		}
 	}
 	
+	boolean validateBST(TreeNode root, int max, int min) {
+		if(root == null) return true;
+		else
+			return (root.getData() > min && root.getData() < max) && validateBST(root.getLeft(), root.getData(), min) && validateBST(root.getRight(), max, root.getData());
+	}
+	
 	public static void main(String[] args) {
 
 		BinarySearchTree2 t = new BinarySearchTree2();
 		
 		TreeNode root = new TreeNode(6);
-		TreeNode inner1 = new TreeNode(5);
+		TreeNode inner1 = new TreeNode(4);
 		TreeNode inner2 = new TreeNode(10);
 		TreeNode leaf1 = new TreeNode(3);
-		TreeNode leaf2 = new TreeNode(4);
+		TreeNode leaf2 = new TreeNode(5);
 		TreeNode leaf3 = new TreeNode(9);
 		TreeNode leaf4 = new TreeNode(11);
 		
@@ -37,6 +43,8 @@ public class BinarySearchTree2 {
 		inner1.setRight(leaf2);
 		inner2.setLeft(leaf3);
 		inner2.setRight(leaf4);
+		
+		System.out.println("Is a Valid Binary Search Tree : " + t.validateBST(root, Integer.MAX_VALUE, Integer.MIN_VALUE));
 		
 		System.out.println("Least common Ancestor");
 		System.out.println(t.findLCA(root, 9, 11).getData());
